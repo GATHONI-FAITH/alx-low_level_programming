ebffb733f5c44b6cd51a61f91c4053d1ea058eda
GATHONI-FAITH <faithgathoni877@gmail.com>
2022-10-19 08:36:32 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -assign a numberto the variable n and print the last digit
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	intn;
+
+	for (n = 0; n < 10; n++)
+	{
+	printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
