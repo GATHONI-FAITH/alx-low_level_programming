b6661eea86d9f1b7d17a3ad868c6c0f7f6e52748
GATHONI-FAITH <faithgathoni877@gmail.com>
2022-11-11 02:32:29 -0800

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: argc parameter
+ * @argv: array of command listed
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
