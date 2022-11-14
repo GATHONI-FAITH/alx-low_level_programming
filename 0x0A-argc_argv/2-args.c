c2945d04eb8a2d502e4be137d58a4d4396d609b1
GATHONI-FAITH <faithgathoni877@gmail.com>
2022-11-13 23:53:35 -0800

    0x0a-agc-argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments its receives
+ * @argc: argc parameter
+ * @argv: array of strings
+ * Return: o on success
+ */
+int main(int argc, char *argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i])
+	}
+	return (0);
+}
