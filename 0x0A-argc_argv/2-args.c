ffa2be7000517f06761a2f3c11ee2a0c129c891d
GATHONI-FAITH <faithgathoni877@gmail.com>
2022-11-14 00:00:25 -0800

    2-argc.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,13 +6,13 @@
  * @argv: array of strings
  * Return: o on success
  */
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[i])
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
